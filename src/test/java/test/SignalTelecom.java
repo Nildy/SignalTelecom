38133895f9a0352f474dc2704b915cda2c83c2fa
MuhammetIsik <62161909+MuhammetIsik@users.noreply.github.com>
2020-06-30 19:57:05 -0400

    Muhammet`s Second commit from website

--- src/test/java/test/SignalTelecom.java
+++ src/test/java/test/SignalTelecom.java
@@ -98,9 +98,10 @@ public class SignalTelecom {
         //Veirfy to see "YOUR ORDER"
         Assert.assertTrue(shopPage.orderincart.getText().contains("CART / $4,999.00"));
 
-        // hi everyone
-
-    // Hi this is melik
+        // hi everyone 
+        //Hello dear friends!!!
+        // Hi from Muhammet!
+        // Hi again from Muhammet
 
     }}
 
