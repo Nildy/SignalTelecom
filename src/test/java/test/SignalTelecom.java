045523d470f0d9b6ec302b7bf099ef6de2e934fe
Arzu Demircan <arzukocdemircan@gmail.com>
2020-06-30 20:06:41 -0400

    arzu

--- src/test/java/test/SignalTelecom.java
+++ src/test/java/test/SignalTelecom.java
@@ -100,15 +100,15 @@ public class SignalTelecom {
 
         // hi everyone 
         //Hello dear friends!!!
-<<<<<<< HEAD
 
-    System.out.println("Sonunda");
 
-=======
+    System.out.println("Sonunda Arzu gonderdi");
+
+
         // Hi from Muhammet!
         // Hi again from Muhammet
         
->>>>>>> 758ff9b76225eced1113e45d4707b0cec141904b
+
     }}
 
 
